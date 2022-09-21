89d1b1f9c23361b91827e641432a71c8ba6d028c
shubham-rawal <shubhamrawal2001@gmail.com>
2022-09-21 23:58:39 +0530

    icons added for facilities and alert showing description of the selected facility.

---
+++ SnowSeeker/Facility.swift
@@ -0,0 +1,47 @@
+//
+//  Facility.swift
+//  SnowSeeker
+//
+//  Created by Shubham Rawal on 21/09/22.
+//
+
+import SwiftUI
+
+struct Facility: Identifiable {
+    let id = UUID()
+    var name: String
+    
+    private let icons = [
+        "Accomodation": "house",
+        "Beginners": "1.circle",
+        "Cross-country": "map",
+        "Eco-friendly": "leaf.arrow.circlepath",
+        "Family": "person.3"
+    ]
+    
+    private let descriptions = [
+        "Accomodation": "This resort has popular on-site accomodation.",
+        "Beginners": "This resort has lots of ski schools.",
+        "Cross-country": "This resort has many cross-country ski routes.",
+        "Eco-friendly": "This resort has won an award for environmental friendliness.",
+        "Family": "This resort is popular with families."
+    ]
+    
+    var icon: some View {
+        if let iconName = icons[name] {
+            return Image(systemName: iconName)
+                .accessibilityLabel(name)
+                .foregroundColor(.secondary)
+        } else {
+            fatalError("Unknown facility type: \(name)")
+        }
+    }
+    
+    var description: String {
+        if let message = descriptions[name] {
+            return message
+        } else {
+            fatalError("Unknown facility type: \(name)")
+        }
+    }
+}
